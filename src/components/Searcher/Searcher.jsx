51a9647560bdfc67f9ee9df547bf73993b62e7ed
Kim Ramirez <kim.rc.1066@gmail.com>
2023-05-09 19:58:18 -0600

    feat: useEffect added

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,11 +1,26 @@
 
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 const API_URL = "https://rickandmortyapi.com/api/character/"
 
+
 const Searcher = () => {
+    
     const [search, setSearch] = useState('');
     const [characters, setCharacters] = useState([]);
+    
+    //useEfect start
+    useEffect(() => {
+        if (search.trim() === '') return;
+        
+        // aqui hago la peticion
+        fetch(`${API_URL}?name=${search}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setCharacters(data.results);
+          });
+      }, [search]);
+       //useEfect end
 
     const handleText = (e) => {
         setSearch(e.target.value.toLowerCase());
